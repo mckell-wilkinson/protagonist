8d9afaea3954286d698647d23551fb4db8714d7a
Daniel <danielvwilkinson@gmail.com>
2020-06-13 16:02:49 +0100

    rendered blogs on the homepage

--- pages/index.vue
+++ pages/index.vue
@@ -1,79 +1,50 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        protaganist
-      </h1>
-      <h2 class="subtitle">
-        Protagnist blog
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
+  <div>
+    <!-- THIS IS THE LAST BLOG POSTED -->
+    <nuxt-link :to="`blog/${blogPosts[0].slug}`">
+      <img :alt="blogPosts[0].title" :src="blogPosts[0].image" />
+      <p>{{ blogPosts[0].subject }}</p>
+      <p>{{ blogPosts[0].title }}</p>
+   <p>{{ new Date(blogPosts[0].date).toDateString() }}</p>
+    </nuxt-link>
+
+    <!-- THESE ARE THE NEXT 8 -->
+    <ul>
+      <li
+        v-for="(blog, index) in blogPosts"
+        :key="blog.title"
+        v-if="index && index < 9"
+      >
+        <nuxt-link :to="`blog/${blog.slug}`">
+          <img :alt="blog.title" :src="blog.image" />
+          <p>{{ blog.subject }}</p>
+          <p>{{ blog.title }}</p>
+          <p>{{ new Date(blog.date).toDateString() }}</p>
+        </nuxt-link>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-
+import Logo from "~/components/Logo.vue";
 
 export default {
   head() {
     return {
-      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+      script: [
+        { src: "https://identity.netlify.com/v1/netlify-identity-widget.js" }
+      ]
     };
   },
-    components: {
+
+  computed: {
+    blogPosts() {
+      return this.$store.state.blogPosts;
+    }
+  },
+  components: {
     Logo
   }
-
 };
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
