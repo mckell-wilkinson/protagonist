f092f7a68a51fca2069b71f68ff2c51ec7575775
Daniel <danielvwilkinson@gmail.com>
2020-06-13 15:10:36 +0100

    initial commit

---
+++ pages/blog/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <article>
+    <h1>{{blogPost.title}}</h1>
+    <div v-html="$md.render(blogPost.body)" />
+  </article>
+</template>
+
+<script>
+
+export default {
+  computed: {
+    blogPosts() {
+      return this.$store.state.blogPosts;
+    },
+  },
+};
+
+</script>
